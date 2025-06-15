a3331dfdfd3ce9ba01f80fa96db9f10e9e0b257c
Lydia Schmidt <155835613+lschmi@users.noreply.github.com>
2025-06-15 17:19:06 -0500

    Create project2.js

---
+++ Chapter2Project/project2.js
@@ -0,0 +1,15 @@
+var myName = 'Lydia Schmidt';
+var para1 = document.getElementById("p1");
+para1.textContent = myName;
+var n1 = 22;
+var n2 = 33;
+var numberSum = n1 + n2;
+document.getElementById("p2").textContent = numberSum;
+var numberMult = n1 * n2;
+document.getElementById("p3").textContent = numberMult;
+var myNameAddNum = n1 + n2 + myName;
+document.getElementById("p4").textContent = myNameAddNum;
+var myNameMultNum = n1 * myName;
+document.getElementById("p5").textContent = myNameMultNum;
+var ageCompare = 28 < n1 * n2;
+document.getElementById("p6").textContent = ageCompare;
