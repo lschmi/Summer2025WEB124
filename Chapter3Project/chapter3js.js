013e1760040fa726038ad38b0c6f0725c562dd37
Lydia Schmidt <155835613+lschmi@users.noreply.github.com>
2025-06-29 12:56:32 -0500

    Create chapter3js.js

---
+++ Chapter3Project/chapter3js.js
@@ -0,0 +1,24 @@
+//Lydia Schmidt 06/27/25
+
+var name = 'Lydia Schmidt'
+console.log(name);
+
+var salary = 68000
+console.log(salary);
+
+var vetStatus = 'No'
+console.log(vetStatus);
+
+const friendsNames = ["Jade", "Terra", "Laurie"];
+console.log(friendsNames);
+
+const friendsSalary = [84000, 63000, 67000];
+console.log(friendsSalary);
+
+
+const objFriend = {
+  firstName: 'Debby',
+  lastName: 'Hong',
+  salary: 72000
+};
+console.log(objFriend);
